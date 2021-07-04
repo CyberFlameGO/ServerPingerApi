222031fb96f3117a1b8b1917fe9b6430b1e217c8
YouHaveTrouble <garrenpolska@gmail.com>
2021-07-04 21:50:26 +0200

    swapped error codes to 200 because cloudflare doesn't like error codes.

--- src/main/java/me/youhavetrouble/ServerPingerApi/PingError.java
+++ src/main/java/me/youhavetrouble/ServerPingerApi/PingError.java
@@ -4,9 +4,9 @@ import org.springframework.http.ResponseEntity;
 
 public enum PingError {
 
-    COULD_NOT_PARSE_PORT(400,"{ error: \"Could not parse the port number\" }"),
-    SERVER_OFFLINE(502, "{ error: \"Server is offline\" }"),
-    TIMED_OUT(504, "{ error: \"Status request timed out\" }");
+    COULD_NOT_PARSE_PORT(200,"{ error: \"Could not parse the port number\" }"),
+    SERVER_OFFLINE(200, "{ error: \"Server is offline\" }"),
+    TIMED_OUT(200, "{ error: \"Status request timed out\" }");
 
     private final ResponseEntity<String> response;
 
